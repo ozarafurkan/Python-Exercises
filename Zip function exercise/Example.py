b04a3966e9348b780b81417227f28e1d60b91d48
ozarafurkan <ozarafurkan@gmail.com>
2020-09-09 22:32:42 +0300

    Update Example.py
    My first exercise solution

---
+++ Zip function exercise/Example.py
@@ -0,0 +1,3 @@
+names =["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+surnames = ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+isimsoyisim=list(zip(names,surnames))
