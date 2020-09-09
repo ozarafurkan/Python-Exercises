4a06a67fb19d9315493cc94edc409d8cd8b61768
ozarafurkan <ozarafurkan@gmail.com>
2020-09-09 23:36:19 +0300

    Update Example.py
    comment line

--- Zip function exercise/Example.py
+++ Zip function exercise/Example.py
@@ -2,4 +2,4 @@ names =["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
 surnames = ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
 nameSurname=list(zip(names,surnames))
 print(nameSurname)
-#print added
+#print added.
