194f0c51e1577295b6a3972d1906aa6cb562dd25
ozarafurkan <ozarafurkan@gmail.com>
2020-09-12 14:29:30 +0300

    Created

---
+++ Decarator function exercises/Ex 1.py
@@ -0,0 +1,29 @@
+def ekstra (fonk):
+    def wrapped(sayilar):
+        print("Mukemmel Sayılar")
+        for i in sayilar:
+            x=1
+            sum=0
+            while (x<i):
+                if i%x==0:
+                    sum+=x
+                x+=1
+            if sum==i:
+                print(i)
+        fonk(sayilar)
+    return wrapped
+
+@ekstra
+def asalliste(sayilar):
+    print("Asal sayılar ... ")
+    asal=True
+    for i in sayilar:
+        if i ==1 :
+            asal=False
+        for j in range(2,i):
+            if (i%j==0 )and( i!=2 )and (i!=j) :
+                asal=False
+                break
+        if asal==True:
+            print(i)
+        asal=True
