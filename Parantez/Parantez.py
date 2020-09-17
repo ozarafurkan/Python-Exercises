581604a420510fff91606cd31cbcf404e6872de3
ozarafurkan <51303286+ozarafurkan@users.noreply.github.com>
2020-09-17 12:32:49 +0300

    Update Parantez.py

--- Parantez/Parantez.py
+++ Parantez/Parantez.py
@@ -9,7 +9,7 @@ for i in range(uzunluk):
         if Sade_islem[i]==j and Sade_islem[i+1]==")" :
             print("Hata1")
         elif Sade_islem[i]==j and (not Sade_islem[i+1].isnumeric() and Sade_islem[i-1].isnumeric() and Sade_islem[i+1]!="("):
-            print("Hata2")(3 * (2+4)) / 2
+            print("Hata2")
     if Sade_islem[i]=="(" and Sade_islem[i+1]==")" :
         print("Hata3")
 
