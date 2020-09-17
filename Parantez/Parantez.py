e403450020bd939664930a10de6c0ac1f34070fc
ozarafurkan <ozarafurkan@gmail.com>
2020-09-17 14:17:59 +0300

    import sys

--- Parantez/Parantez.py
+++ Parantez/Parantez.py
@@ -1,3 +1,5 @@
+import sys
+
 islem=input()
 sembol=["/","*","+","-"]
 
@@ -8,10 +10,13 @@ for i in range(uzunluk):
     for j in sembol:
         if Sade_islem[i]==j and Sade_islem[i+1]==")" :
             print("Hata1")
+            sys.exit() # Diğer hata mesajlarını göstermeyi engellemek için
         elif Sade_islem[i]==j and (not Sade_islem[i+1].isnumeric() and Sade_islem[i-1].isnumeric() and Sade_islem[i+1]!="("):
-            print("Hata2")(3 * (2+4)) / 2
+            print("Hata2")
+            sys.exit()
     if Sade_islem[i]=="(" and Sade_islem[i+1]==")" :
         print("Hata3")
+        sys.exit()
 
 if islem.count("(") != islem.count(")"):
      print("Hata4")
