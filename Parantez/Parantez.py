344b2891bb6c48e7bcfe6793e65e13f7eb5672a9
ozarafurkan <ozarafurkan@gmail.com>
2020-09-17 16:32:22 +0300

    Hata açıklamaları

--- Parantez/Parantez.py
+++ Parantez/Parantez.py
@@ -9,14 +9,14 @@ uzunluk=len(Sade_islem)
 for i in range(uzunluk):
     for j in sembol:
         if Sade_islem[i]==j and Sade_islem[i+1]==")" :
-            print("Hata1")
+            print("Hata1(Parantez işlemsiz kapandı)  {}. karakter ".format(i+1))
             sys.exit() # Diğer hata mesajlarını göstermeyi engellemek için
         elif Sade_islem[i]==j and (not Sade_islem[i+1].isnumeric() and Sade_islem[i-1].isnumeric() and Sade_islem[i+1]!="("):
-            print("Hata2")
+            print("Hata2 {}. karakter ".format(i+1))
             sys.exit()
     if Sade_islem[i]=="(" and Sade_islem[i+1]==")" :
-        print("Hata3")
+        print("Hata3(Boş parantez) {}. karakter ".format(i+1))
         sys.exit()
 
 if islem.count("(") != islem.count(")"):
-     print("Hata4")
+     print("Hata4(Fazladan parantez)")
