321e87b44647658c8560367f9869ee0fd6ec49ff
ozarafurkan <ozarafurkan@gmail.com>
2020-09-19 20:39:32 +0300

    Solved

---
+++ Hacker rank solutions (pythonic)/Finding the percentage/Finding the percentage.py
@@ -0,0 +1,7 @@
+marks={}
+for i in range(int(input())):
+    name,*mark=input().split()
+    mark=list(map(float,mark))
+    marks[name]=mark
+print("{:.2f}".format( sum(marks[input()])/3 ) )
+#print("%.2f"%( sum(marks[input()])/3 ) )
